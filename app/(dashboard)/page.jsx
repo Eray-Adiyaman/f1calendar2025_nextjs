7c3e1c2708cb7a23e873b674f7c0922ac04e7ac3
Eray-Adiyaman <erayadiyaman@outlook.com>
2024-02-06 18:42:49 +0300

    calendar json(half), date formatting setup

--- app/(dashboard)/page.jsx
+++ app/(dashboard)/page.jsx
@@ -1,6 +1,13 @@
+import { parseISO, format } from "date-fns"
 
+/* date format tests for accordions*/
 export default function HomePage() {
+  const date1 = parseISO("2024-03-01T12:30:00+0000")
   return (
+    <>
     <div>HomePage</div>
+    <div>{format(date1,"LLLL d, EEEE h:m a")}</div>
+    </>
+
   )
 }
