d73419c03d702d2b76c8dbe55ddfad66aa968352
Eray-Adiyaman <erayadiyaman@outlook.com>
2024-02-13 17:20:20 +0300

    race info formats

--- app/components/Accordion.jsx
+++ app/components/Accordion.jsx
@@ -1,45 +1,64 @@
 import { format } from "date-fns";
 
 export default function Accordion({ race }) {
-  //console.log(race.dates[5].springsession === undefined by default, fill later when data available ) 
+
+  const sprintTrue = race.sprintWeekend ? "visible" : "hidden";
+  const sprintFalse = race.sprintWeekend ? "hidden" : "visible";
+
+  const formatTime = (indexparam) => {
+    switch (indexparam) {
+      case 0:
+        return race.dates[0].raceStart
+          ? format(race.dates[0].raceStart, "LLLL d, EEEE H:mm a")
+          : " TBD";
+      case 1:
+        return race.dates[1].qualifySession
+          ? format(race.dates[1].qualifySession, "LLLL d, EEEE H:mm a")
+          : " TBD";
+      case 2:
+        return race.dates[2].freepractice3
+          ? format(race.dates[2].freepractice3, "LLLL d, EEEE H:mm a")
+          : " TBD";
+      case 3:
+        return race.dates[3].freepractice2
+          ? format(race.dates[3].freepractice2, "LLLL d, EEEE H:mm a")
+          : " TBD";
+      case 4:
+        return race.dates[4].freepractice1
+          ? format(race.dates[4].freepractice1, "LLLL d, EEEE H:mm a")
+          : " TBD";
+      case 5:
+         return race.dates[5].sprintShootout
+         ? format(race.dates[5].sprintShootout, "LLLL d, EEEE H:mm a")
+         : " TBD";
+      case 6:
+        return race.dates[6].sprintStart
+         ? format(race.dates[6].sprintStart, "LLLL d, EEEE H:mm a")
+         : " TBD";
+      default:
+        break;
+    }
+  };
+
   return (
-    <>
-      <div className="accord" key={race.id}>
-        <h2>
-          {race.gpName} round-{race.id} {race.sprintWeekend ? "/ Sprint Weekend*" : ""}
-        </h2>
-        <h6>
-          Star--
-          {race.dates[0].raceStart
-            ? format(race.dates[0].raceStart, "LLLL d, EEEE H:mm a")
-            : " TBD"}
-        </h6>
-        <h6>
-          Qual-
-          {race.dates[1].qualifySession
-            ? format(race.dates[1].qualifySession, "LLLL d, EEEE H:mm a")
-            : " TBD"}
-        </h6>
-        <h6>
-          FP3--
-          {race.dates[2].freepractice3
-            ? format(race.dates[2].freepractice3, "LLLL d, EEEE H:mm a")
-            : " TBD"}
-        </h6>
-        <h6>
-          FP2--
-          {race.dates[3].freepractice2
-            ? format(race.dates[3].freepractice2, "LLLL d, EEEE H:mm a")
-            : " TBD"}
-        </h6>
-        <h6>
-          FP1--
-          {race.dates[4].freepractice1
-            ? format(race.dates[4].freepractice1, "LLLL d, EEEE H:mm a")
-            : " TBD"}
-        </h6>
-        <p>----------------------------------------------------------</p>
-      </div>
-    </>
+    <div className="accord">
+      <li>
+        <button>
+          {race.gpName}
+          <span> /— {race.sprintWeekend ? "Sprint Weekend" : ""}</span>
+        </button>
+        <div>
+          <ul>
+            <li>Race Start- {formatTime(0)}</li>
+            <li className={sprintTrue} >SPRINT RACE- {formatTime(6)}</li>
+            <li className={sprintTrue} >Sprint Shootout(sprint quali)- {formatTime(5)}</li>
+            <li>Qualifying Session- {formatTime(1)}</li>
+            <li className={sprintFalse}>FP3- {formatTime(2)} </li>
+            <li className={sprintFalse}>FP2- {formatTime(3)}</li>
+            <li>FP1- {formatTime(4)}</li>
+          </ul>
+        </div>
+      </li>
+    </div>
   );
 }
--- app/components/PointsTable.jsx
+++ app/components/PointsTable.jsx
@@ -1,93 +1,93 @@
-
-
 export default function PointsTable() {
   return (
     <div className="pointstable">
       <table>
-        <tr>
-          <th>Drivers</th>
-          <th>Points</th>
-        </tr>
-        <tr>
-          <td>Driver Name</td>
-          <td> -123</td>
-        </tr>
-        <tr>
-          <td>Driver Name</td>
-          <td> -123</td>
-        </tr>
-        <tr>
-          <td>Driver Name</td>
-          <td> -123</td>
-        </tr>
-        <tr>
-          <td>Driver Name</td>
-          <td> -123</td>
-        </tr>
-        <tr>
-          <td>Driver Name</td>
-          <td> -123</td>
-        </tr>
-        <tr>
-          <td>Driver Name</td>
-          <td> -123</td>
-        </tr>
-        <tr>
-          <td>Driver Name</td>
-          <td> -123</td>
-        </tr>
-        <tr>
-          <td>Driver Name</td>
-          <td> -123</td>
-        </tr>
-        <tr>
-          <td>Driver Name</td>
-          <td> -123</td>
-        </tr>
-        <tr>
-          <td>Driver Name</td>
-          <td> -123</td>
-        </tr>
-        <tr>
-          <td>Driver Name</td>
-          <td> -123</td>
-        </tr>
-        <tr>
-          <td>Driver Name</td>
-          <td> -123</td>
-        </tr>
-        <tr>
-          <td>Driver Name</td>
-          <td> -123</td>
-        </tr>
-        <tr>
-          <td>Driver Name</td>
-          <td> -123</td>
-        </tr>
-        <tr>
-          <td>Driver Name</td>
-          <td> -123</td>
-        </tr>
-        <tr>
-          <td>Driver Name</td>
-          <td> -123</td>
-        </tr>
-        <tr>
-          <td>Driver Name</td>
-          <td> -123</td>
-        </tr>
-        <tr>
-          <td>Driver Name</td>
-          <td> -123</td>
-        </tr>
-        <tr>
-          <td>Driver Name</td>
-          <td> -123</td>
-        </tr>
-        <tr>
-          <td>Driver Name</td>
-          <td> -123</td>
-        </tr>
+        <tbody>
+          <tr>
+            <th>Drivers</th>
+            <th>Points</th>
+          </tr>
+          <tr>
+            <td>Driver Name</td>
+            <td> -123</td>
+          </tr>
+          <tr>
+            <td>Driver Name</td>
+            <td> -123</td>
+          </tr>
+          <tr>
+            <td>Driver Name</td>
+            <td> -123</td>
+          </tr>
+          <tr>
+            <td>Driver Name</td>
+            <td> -123</td>
+          </tr>
+          <tr>
+            <td>Driver Name</td>
+            <td> -123</td>
+          </tr>
+          <tr>
+            <td>Driver Name</td>
+            <td> -123</td>
+          </tr>
+          <tr>
+            <td>Driver Name</td>
+            <td> -123</td>
+          </tr>
+          <tr>
+            <td>Driver Name</td>
+            <td> -123</td>
+          </tr>
+          <tr>
+            <td>Driver Name</td>
+            <td> -123</td>
+          </tr>
+          <tr>
+            <td>Driver Name</td>
+            <td> -123</td>
+          </tr>
+          <tr>
+            <td>Driver Name</td>
+            <td> -123</td>
+          </tr>
+          <tr>
+            <td>Driver Name</td>
+            <td> -123</td>
+          </tr>
+          <tr>
+            <td>Driver Name</td>
+            <td> -123</td>
+          </tr>
+          <tr>
+            <td>Driver Name</td>
+            <td> -123</td>
+          </tr>
+          <tr>
+            <td>Driver Name</td>
+            <td> -123</td>
+          </tr>
+          <tr>
+            <td>Driver Name</td>
+            <td> -123</td>
+          </tr>
+          <tr>
+            <td>Driver Name</td>
+            <td> -123</td>
+          </tr>
+          <tr>
+            <td>Driver Name</td>
+            <td> -123</td>
+          </tr>
+          <tr>
+            <td>Driver Name</td>
+            <td> -123</td>
+          </tr>
+          <tr>
+            <td>Driver Name</td>
+            <td> -123</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
